89e6d6d12fb053012d174a1b4cc05eb87261e6de
TatianaArrieta <113394215+TatianaArrieta@users.noreply.github.com>
2022-11-26 11:31:08 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -12,22 +12,26 @@ from datetime import datetime
 
 def clean_data():
     
-    df = pd.read_csv("solicitudes_credito.csv", sep=";")
+def clean_data():
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
+    
+
     df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
 
     for column in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
-        df.column = df.column.str.lower()
-        df.column = df.column.apply(lambda x: x.replace('_', ' '))
-        df.column = df.column.apply(lambda x: x.replace('-', ' '))
+        df[column] = df[column].str.lower()
+        df[column] = df[column].apply(lambda x: x.replace('_', ' '))
+        df[column] = df[column].apply(lambda x: x.replace('-', ' '))
 
-    df.comuna_ciudadano = df.comuna_ciudadano.astype(float)
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\$[\s*]", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace(",", "")
+    df['monto_del_credito'] = df['monto_del_credito'].str.replace("\.00", "")
+    df['monto_del_credito'] = df['monto_del_credito'].astype(int)
+    
+    df['comuna_ciudadano'] = df['comuna_ciudadano'].astype(float)
+
     df['fecha_de_beneficio'] = df['fecha_de_beneficio'].apply(lambda x: datetime.strptime(x, "%Y/%m/%d") if (len(re.findall("^\d+/", x)[0]) - 1) == 4 else datetime.strptime(x, "%d/%m/%Y"))
-
-    df.monto_del_credito = df.monto_del_credito.str.replace("\$[\s*]", "")
-    df.monto_del_credito = df.monto_del_credito.str.replace(",", "")
-    df.monto_del_credito = df.monto_del_credito.str.replace("\.00", "")
-    df.monto_del_credito = df.monto_del_credito.astype(int)
 
     df.drop_duplicates(inplace = True)
     
