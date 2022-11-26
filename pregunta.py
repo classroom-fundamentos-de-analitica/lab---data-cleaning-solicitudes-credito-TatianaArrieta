299ef1f9554d04d8e81624bb4fc59a3cb2ea1d13
TatianaArrieta <113394215+TatianaArrieta@users.noreply.github.com>
2022-11-26 10:58:09 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -11,17 +11,15 @@ import pandas as pd
 
 def clean_data():
 
-    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
-    df.monto_del_credito = df.monto_del_credito.str.strip("$")
-    df.monto_del_credito = df.monto_del_credito.str.replace(',','')
-    df.monto_del_credito = df.monto_del_credito.astype(float)
-
     df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]] = df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
     df = df.replace(to_replace = "(_)|(-)", value = " ", regex = True)    
-    #df = df.replace(to_replace = "[,$]|(\.00$)", value = "", regex = True)
+    df = df.replace(to_replace = "[,$]|(\.00$)", value = "", regex = True)
+
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
 
     df.dropna(axis = 0, inplace = True)
     df.drop_duplicates(inplace = True)
-    
+
     return df
