63b6403e167cdc12f4f478805a747b68a0c74054
TatianaArrieta <113394215+TatianaArrieta@users.noreply.github.com>
2022-11-26 10:38:04 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -13,8 +13,15 @@ def clean_data():
 
     df = pd.read_csv("solicitudes_credito.csv", sep=";")
 
-    #
-    # Inserte su código aquí
-    #
+    df.comuna_ciudadano = df.comuna_ciudadano.astype(int)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
+    df.monto_del_credito = df.monto_del_credito.str.replace("[,$]|(\.00$)","")
+
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]] = df[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
+    df = df.replace(to_replace = "(_)|(-)", value = " ", regex = True)    
+
+    df.dropna(axis = 0, inplace = True)
+    df.drop_duplicates(inplace = True)
 
     return df
